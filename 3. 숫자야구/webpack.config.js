f4128a102df60ecc6644ab48678de5fc2b1a655d
jimin7777 <jm040629@gmail.com>
2021-05-05 21:29:40 +0900

    3-1 웹팩 사용하기

---
+++ 3. 숫자야구/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: {
+        app: './main.js',
+    },
+    module: {
+        rules: [{
+            
+        }],
+    },
+    plugins: {},
+    output: {
+        fillname: '[name].js',//=app.js
+        path: '.dist',
+    },
+};
