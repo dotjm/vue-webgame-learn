25447dba3ee7c983e87d390ff6c320da1d60c175
jimin7777 <jm040629@gmail.com>
2022-01-18 00:54:50 +0900

    3-4 v-for로 반복문 사용하기

--- 3. 숫자야구/NumberBaseball.vue
+++ 3. 숫자야구/NumberBaseball.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <h1>{{result}}</h1>
-        <form v-on:submit="onSubmitForm">
+        <form @submit.prevent="onSubmitForm"> <!--v-on:submit-->
             <input ref="answer" maxlength="4" v-model="value" />
             <button>입력</button>
         </form>
         <div>시도: {{tries.length}}</div>
         <ul>
-            <li v-for="t in tries">{{t}}</li>
+            <li v-for="t in tries">
+                <div>{{t.try}}</div>
+                <div>{{t.result}}</div>
+            </li>
         </ul>
     </div>
 </template>
@@ -23,8 +26,11 @@
         }, 
         methods: {
             onSubmitForm(e) {
-                e.preventDefault();
-                this.tries.push(this.value);
+                //e.preventDefault();
+                this.tries.push({
+                    try: this.value,
+                    result: '홈런',
+                });
                 this.value = '';
                 this.$refs.answer.focus();
             }
