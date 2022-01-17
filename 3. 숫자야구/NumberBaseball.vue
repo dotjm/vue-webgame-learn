a3e4a217a4f68ddc36fbd25157019096c102ba02
jimin7777 <jm040629@gmail.com>
2022-01-18 00:33:38 +0900

    3-2 프로젝트 구조와 웹팩 빌드

---
+++ 3. 숫자야구/NumberBaseball.vue
@@ -0,0 +1,31 @@
+//자바스크립트 파일로 생각할 것
+<template>
+    <div>
+        <h1>{{result}}</h1>
+        <form b-on:submit="onSubmitForm">
+            <input ref="answer" maxlength="4" v-model="value">
+            <button>입력</button>
+        </form>
+        <div>시도: {{}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                value: '',
+                result: '',
+            }
+        }, 
+        methods: {
+            onSubmitForm(e) {
+                e.preentDefault();
+            }
+        }
+    };
+</script>
+
+<style>
+
+</style>
